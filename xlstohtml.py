63f14a92a0a0b813b31716baa2fb337c0344e445
Ivanopulos <Pochta.rus.lc@gmail.com>
2023-05-31 14:03:42 +0300

    место ошибки с несуществующей меткой

--- xlstohtml.py
+++ xlstohtml.py
@@ -1,17 +1,42 @@
-# для нарезки хтмл-ок по шаблону в эксельке, один столбец- один файл рождается
+# для нарезки хтмл-ок по шаблону в эксельке, один столбец-один файл
 import pandas  # +openpyxl
 import copy
-df = pandas.read_excel("1.xlsx", header=None)
-#df.iloc[0] = df.iloc[0].str.replace(r'\.(?!h)', "", regex=True)
-#df.iloc[0] = df.iloc[0].str.replace(r'[,/\\\?\!]', "", regex=True)
-df.iloc[0] = df.iloc[0].str.lower()
+df = pandas.read_excel("1.xlsm", header=None)
+df.iloc[0] = df.iloc[0].str.lower()  # хз зачем но очень нада
 df.columns = df.iloc[0]
 #df = df.fillna("-")
+# Функция для замены пустых ячеек на "-"
+def fill_empty_cells(row):
+    not_empty_count = 0
+    for i, cell in enumerate(row):
+        if pandas.notna(cell):
+            not_empty_count += 1
+            if not_empty_count >= 5:
+                break
+        else:
+            not_empty_count = 0
+    if not_empty_count >= 5:
+        row = row.fillna('-')
+    return row
+def fill_empty_cells1(row):
+    not_empty_count = 0
+    for i, cell in enumerate(row):
+        if pandas.notna(cell):
+            not_empty_count += 1
+            if not_empty_count >= 7:
+                break
+        else:
+            not_empty_count = 0
+    if not_empty_count >= 7:
+        row = row.fillna('-')
+    return row
+df.iloc[172:294] = df.iloc[172:294].apply(fill_empty_cells, axis=1)
+df.iloc[5296:5385] = df.iloc[5296:5385].apply(fill_empty_cells1, axis=1)
 print(df.columns.values[1])
-colcount = df.count(axis='columns')[0]   # один столбец под разметку один под названия
-with open("1.html", 'r', encoding='utf-8') as f:  # save before chenge
+colcount = df.count(axis='columns')[0]  # &&
+with open("1.html", 'r', encoding='utf-8') as f:  # save before change
     get_all2 = f.readlines()
-for fl in range(2, colcount):  # перебор столбцов и нарезка их на файлы
+for fl in range(3, colcount+1):  # перебор столбцов и нарезка их на файлы
     nstolb = df.columns.values[fl]
     print(nstolb)
     mem = 0
@@ -39,7 +64,12 @@ for fl in range(2, colcount):  # перебор столбцов и нарезк
                     tx = str(tx).replace(".", ",")
                 except:
                     asd = 0
-                get_all[isch] = get_all[isch][:usch] + tx + get_all[isch][usch + len(found):]
+                try:
+                    get_all[isch] = get_all[isch][:usch] + str(tx) + get_all[isch][usch + len(found):]
+                except:
+                    print(tx)
+                    print(found)
+                    get_all[isch] = get_all[isch][:usch] + str(tx) + get_all[isch][usch + len(found):]
                 found = ""
             usch = usch - 1
         isch = isch + 1
