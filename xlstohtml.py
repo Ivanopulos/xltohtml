8634b1bf41790a111b5e4ea585b3c6056a8b1563
Ivax <68695142+Ivanopulos@users.noreply.github.com>
2023-06-26 12:07:32 +0300

    Update xlstohtml.py

--- xlstohtml.py
+++ xlstohtml.py
@@ -1,4 +1,3 @@
-# для нарезки хтмл-ок по шаблону в эксельке, один столбец-один файл
 import pandas  # +openpyxl
 import copy
 df = pandas.read_excel("1.xlsm", header=None)
@@ -6,35 +5,35 @@ df.iloc[0] = df.iloc[0].str.lower()  # хз зачем но очень нада
 df.columns = df.iloc[0]
 #df = df.fillna("-")
 # Функция для замены пустых ячеек на "-"
-def fill_empty_cells(row):
-    not_empty_count = 0
-    for i, cell in enumerate(row):
-        if pandas.notna(cell):
-            not_empty_count += 1
-            if not_empty_count >= 5:
-                break
-        else:
-            not_empty_count = 0
-    if not_empty_count >= 5:
-        row = row.fillna('-')
-    return row
-def fill_empty_cells1(row):
-    not_empty_count = 0
-    for i, cell in enumerate(row):
-        if pandas.notna(cell):
-            not_empty_count += 1
-            if not_empty_count >= 7:
-                break
-        else:
-            not_empty_count = 0
-    if not_empty_count >= 7:
-        row = row.fillna('-')
-    return row
-df.iloc[172:294] = df.iloc[172:294].apply(fill_empty_cells, axis=1)
-df.iloc[5296:5385] = df.iloc[5296:5385].apply(fill_empty_cells1, axis=1)
+# def fill_empty_cells(row):
+#     not_empty_count = 0
+#     for i, cell in enumerate(row):
+#         if pandas.notna(cell):
+#             not_empty_count += 1
+#             if not_empty_count >= 5:
+#                 break
+#         else:
+#             not_empty_count = 0
+#     if not_empty_count >= 5:
+#         row = row.fillna('-')
+#     return row
+# def fill_empty_cells1(row):
+#     not_empty_count = 0
+#     for i, cell in enumerate(row):
+#         if pandas.notna(cell):
+#             not_empty_count += 1
+#             if not_empty_count >= 7:
+#                 break
+#         else:
+#             not_empty_count = 0
+#     if not_empty_count >= 7:
+#         row = row.fillna('-')
+#     return row
+# df.iloc[172:294] = df.iloc[172:294].apply(fill_empty_cells, axis=1)
+# df.iloc[5296:5385] = df.iloc[5296:5385].apply(fill_empty_cells1, axis=1)
 print(df.columns.values[1])
 colcount = df.count(axis='columns')[0]  # &&
-with open("1.html", 'r', encoding='utf-8') as f:  # save before change
+with open("1.html", 'r', encoding='utf-8') as f:  # save before chenge
     get_all2 = f.readlines()
 for fl in range(3, colcount+1):  # перебор столбцов и нарезка их на файлы
     nstolb = df.columns.values[fl]
@@ -63,7 +62,7 @@ for fl in range(3, colcount+1):  # перебор столбцов и нарез
                     if pandas.isna(tx):
                         tx = ""
                     # check if tx is a date
-                    elif pandas.to_datetime(tx, errors='coerce') is not pandas.NaT:
+                    elif not isinstance(tx, (int, float)) and pandas.to_datetime(tx, errors='coerce') is not pandas.NaT:
                         tx = pandas.to_datetime(tx).strftime("%d.%m.%Y")
                     else:
                         float(tx)
@@ -81,7 +80,7 @@ for fl in range(3, colcount+1):  # перебор столбцов и нарез
             usch = usch - 1
         isch = isch + 1
     #/n делит одноименных б
-    my_file = open(df.iloc[0][fl], "w",  encoding="utf-8")
+    my_file = open("/home/" + df.iloc[0][fl], "w",  encoding="utf-8")
     my_file.writelines(get_all)
     my_file.close()
 print("FINISH")
